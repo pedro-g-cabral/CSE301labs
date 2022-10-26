38e66aaf3a6e425d69f2799dbac884378894e0e4
Nguyễn Doãn Đại <40011497+MegumiTadokoro@users.noreply.github.com>
2022-10-26 23:07:13 +0200

    Add more tests for beta-reduction

--- Project/Eval.hs
+++ Project/Eval.hs
@@ -35,6 +35,22 @@ theta = A (L "x" (L "y" (A (V "y") (A (A (V "x") (V "x")) (V "y"))))) (L "z" (L 
 
 y = L "f" (A (L "x" (A (V "f") (A (V "x") (V "x")))) (L "y" (A (V "f") (A (V "y") (V "y")))))
 
+-- Exercises taken from https://www.cs.umd.edu/class/fall2017/cmsc330/tests/prac8-soln-fall13.pdf
+-- expect a a
+example4 = A (A (L "z" (V "z")) (L "y" (A (V "y") (V "y")))) (L "x" (A (V "x") (V "a")))
+
+-- expect a a
+example5 = A (A (L "x" (L "y" (A (A (V "x") (V "y")) (V "y")))) (L "a" (V "a"))) (V "b")
+
+-- expect z t
+example6 = A (L "x" (A (L "y" (A (V "x") (V "y"))) (V "t"))) (V "z")
+
+-- expect \y. y
+example7 = A (A (L "x" (A (V "x") (V "x"))) (L "y" (V "y"))) (L "z" (V "z"))
+
+-- expect w
+example8 = A (A (L "x" (L "y" (A (V "x") (V "y")))) (L "z" (V "z"))) (V "w")
+
 subexp :: LExp -> [LExpPtr]
 subexp (V v) = [(Hole, V v)]
 subexp (A e1 e2) =
